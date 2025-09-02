d30e0fa01e4ce63c1c1076c1ada8f0123be4fc7c
lakshya8057 <lakshya8057@gmail.com>
2025-09-02 11:16:07 +0530

    completed the odatamodel.read

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -5,8 +5,68 @@ sap.ui.define([
 
     return Controller.extend("project3.controller.View1", {
         onInit() {
+
+            var odataModel = this.getOwnerComponent().getModel();
+            if (!odataModel) {
+                console.error("OData model is not loaded");
+                return;
+            }
+            //count products
+            odataModel.read("/Products/$count", {
+                success: (oData) => {
+                    this.getView().byId("total_products").setText(oData);
+                },
+                error: (err) => {
+                    this.getView().byId("total_products").setText("0");
+                }
+            });
+            //count customers
+            odataModel.read("/Customers/$count", {
+                success: (oData) => {
+                    this.getView().byId("total_customers").setText(oData);
+                },
+                error: (err) => {
+                    this.getView().byId("total_customers").setText("0");
+                }
+            });
+            //count employees
+            odataModel.read("/Employees/$count", {
+                success: (oData) => {
+                    this.getView().byId("total_employees").setText(oData);
+                },
+                error: (err) => {
+                    this.getView().byId("total_employees").setText("0");
+                }
+            });
+            //count orders
+            odataModel.read("/Orders/$count", {
+                success: (oData) => {
+                    this.getView().byId("total_orders").setText(oData);
+                },
+                error: (err) => {
+                    this.getView().byId("total_orders").setText("0");
+                }
+            });
+            //count invoices
+            odataModel.read("/Invoices/$count", {
+                success: (oData) => {
+                    this.getView().byId("total_invoices").setText(oData);
+                },
+                error: (err) => {
+                    this.getView().byId("total_invoices").setText("0");
+                }
+            });
+            //count suppliers
+            odataModel.read("/Suppliers/$count", {
+                success: (oData) => {
+                    this.getView().byId("total_suppliers").setText(oData);
+                },
+                error: (err) => {
+                    this.getView().byId("total_suppliers").setText("0");
+                }
+            });
         },
-         getRouter: function () {
+        getRouter: function () {
             return this.getOwnerComponent().getRouter();
         },
 
